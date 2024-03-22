f866477975bf45fc9f5b9159a307ee60cb48bad4
avanthik33 <avanthik33@gmail.com>
2024-03-22 15:03:33 +0530

    .\.git\

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,17 +15,33 @@ const Login = () => {
     axios
       .post("http://localhost:5000/api/User/signin", formData)
       .then((response) => {
-        console.log(response.data.message);
+        alert(response.data.message);
       });
   };
   return (
     <div>
       <h2>Login</h2>
       <lable className="form-label">Username</lable>
-      <input type="text" className="form-control" name="username" value={formData.username} onChange={handleChange} />
-      <label htmlFor="" className="form-label">password</label>
-      <input type="text" className="form-control" name="password" value={formData.password} onChange={handleChange} />
-      <button className="btn btn-success" onClick={handleSubmit}>click me for submit</button>
+      <input
+        type="text"
+        className="form-control"
+        name="username"
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <label htmlFor="" className="form-label">
+        password
+      </label>
+      <input
+        type="text"
+        className="form-control"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <button className="btn btn-success" onClick={handleSubmit}>
+        click me for submit
+      </button>
     </div>
   );
 };
